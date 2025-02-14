4e2c81ebd4659e50c4d62277d45992d070616485
Mike <Skvm@users.noreply.github.com>
2025-02-14 21:33:30 +0000

    Quartz sync: Feb 14, 2025, 9:33 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/skvm",
     },
   }),
 }
@@ -27,7 +26,14 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.Explorer(),
+    Component.Explorer(
+    {
+    title: "All Posts",
+    }),
+    Component.DesktopOnly(Component.RecentNotes({
+      title: "Most recent",
+      limit: 5
+    }))
   ],
   right: [
     Component.Graph(),
@@ -44,7 +50,14 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.Explorer(),
+    Component.Explorer(
+    {
+    title: "All Posts",
+    }),
+    Component.DesktopOnly(Component.RecentNotes({
+      title: "Most recent",
+      limit: 5
+    }))
   ],
   right: [],
 }
